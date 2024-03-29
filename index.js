8dbaa41b5e15b94a8622bbe9c03837afec06a2ea
joanne <joannembula33@gmail.com>
2024-03-29 15:44:39 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function show(value){
+    console.log(value);
+  }
+
+  function saturdayFun(someThing = "roller-skate"){
+    return `This Saturday, I want to ${someThing}!`;
+  }
+n
+  function mondayWork(toDo = "go to the office"){
+    return `This Monday, I will ${toDo}.`;
+  }
+
+
+function wrapAdjective(mywrap = "*"){
+   function inner (single = "special"){
+        return `You are ${mywrap}${single}${mywrap}!`;
+    };
+
+    return inner;
+}
